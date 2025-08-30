5ab3dae66b504ccf68c4f1097ce096c04bc34f52
= <qqwobble@gmail.com>
2025-08-31 01:08:35 +0200

    Added span type
    - I miss c++20. :(
    - anyways, have a simple span type that works with c++20-range-like
      types

---
+++ gdextension/cpp/magix_vm/src/magix_vm/ranges.hpp
@@ -0,0 +1,64 @@
+#ifndef MAGIX_RANGES_HPP_
+#define MAGIX_RANGES_HPP_
+
+#include <iterator>
+#include <type_traits>
+#include <utility>
+
+namespace magix::_detail::ranges
+{
+template <class R>
+constexpr auto
+_begin(R &&r)
+{
+    using std::begin;
+    return begin(r);
+}
+
+template <class R>
+constexpr auto
+_end(R &&r)
+{
+    using std::end;
+    return end(r);
+}
+} // namespace magix::_detail::ranges
+
+namespace magix::ranges
+{
+
+template <class T>
+constexpr T *
+to_address(T *ptr)
+{
+    return ptr;
+}
+
+template <class T>
+constexpr auto
+to_address(const T &t)
+{
+    return to_address(t.operator->());
+}
+
+template <class R>
+constexpr auto
+begin(R &&r)
+{
+    return magix::_detail::ranges::_begin(r);
+}
+
+template <class R>
+constexpr auto
+end(R &&r)
+{
+    return magix::_detail::ranges::_end(r);
+}
+
+template <class It> using iter_reference_t = decltype(*std::declval<It &>());
+template <class R> using range_iter_t = decltype(ranges::begin(*std::declval<R &>()));
+template <class R> using range_reference_t = decltype(*ranges::begin(std::declval<R &>()));
+
+} // namespace magix::ranges
+
+#endif // MAGIX_RANGES_HPP_
---
+++ gdextension/cpp/magix_vm/src/magix_vm/span.hpp
@@ -0,0 +1,81 @@
+#ifndef MAGIX_SPAN_HPP_
+#define MAGIX_SPAN_HPP_
+
+#include <cstddef>
+#include <type_traits>
+
+#include "magix_vm/ranges.hpp"
+
+namespace magix
+{
+
+template <class T> class span
+{
+  public:
+    using element_type = T;
+    using value_type = std::remove_cv_t<T>;
+    using pointer = T *;
+    using const_pointer = const T *;
+
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+
+    constexpr span() noexcept {}
+    constexpr span(T *pointer, size_type size) noexcept : ptr{pointer}, length{size} {}
+
+    template <std::size_t N> constexpr span(element_type (&arr)[N]) noexcept : ptr{&arr[0]}, length{N} {}
+
+    template <class ItBeg, class SentEnt>
+    constexpr span(ItBeg it, SentEnt end) noexcept : ptr{magix::ranges::to_address(it)}, length(end - it)
+    {}
+    template <class Range> constexpr span(Range &&range) : span(magix::ranges::begin(range), magix::ranges::end(range)) {}
+
+    constexpr span(const span &) = default;
+
+    constexpr element_type &
+    operator[](size_type i) const noexcept
+    {
+        return ptr[i];
+    }
+
+    constexpr pointer
+    data() const noexcept
+    {
+        return ptr;
+    }
+
+    constexpr pointer
+    begin() const noexcept
+    {
+        return ptr;
+    }
+
+    constexpr pointer
+    end() const noexcept
+    {
+        return ptr + length;
+    }
+
+    constexpr size_type
+    size() const noexcept
+    {
+        return length;
+    }
+
+    constexpr span<const T>
+    as_const() const noexcept
+    {
+        return *this;
+    }
+
+  private:
+    T *ptr = nullptr;
+    size_type length = 0;
+};
+template <class ItBeg, class SentEnt> span(ItBeg, SentEnt) -> span<std::remove_reference_t<ranges::iter_reference_t<ItBeg>>>;
+template <class T, size_t N> span(T (&)[N]) -> span<T>;
+template <class Range> span(Range &&) -> span<std::remove_reference_t<ranges::range_reference_t<Range>>>;
+
+} // namespace magix
+
+#endif // MAGIX_SPAN_HPP_
