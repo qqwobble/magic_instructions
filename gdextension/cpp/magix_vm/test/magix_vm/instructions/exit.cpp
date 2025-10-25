d394539072faba58c07db24fe010e56964ea3f27
= <qqwobble@gmail.com>
2025-10-26 00:17:23 +0200

    Added unit test for instruction exit

---
+++ gdextension/cpp/magix_vm/test/magix_vm/instructions/exit.cpp
@@ -0,0 +1,14 @@
+#include "magix_vm/instructions/instruction_test_macros.hpp"
+
+TEST_SUITE("instructions/exit")
+{
+
+    MAGIX_TEST_CASE_EXECUTE_COMPARE(
+        "exit", UR"(
+@entry:
+exit
+__unittest.put.u32 $0 ; never executed
+)",
+        // no output
+    );
+}
